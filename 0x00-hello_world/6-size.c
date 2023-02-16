9ebd65b11c3834334b3aeea9dd5a88d8c99befff
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2023-02-16 15:08:54 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", size of (char));
+printf("Size of an int: %zu byte(s)\n",size of (int));
+printf("Size of long int: %zu byte(s)\n", size of (long int));
+printf("Size of a float: %zu byte(s)\n", size of (float));
+return (0);
+} 
