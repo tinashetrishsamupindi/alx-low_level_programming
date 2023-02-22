c6cd77480649f5544211cd8149cf029005701cf3
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2023-02-22 05:32:39 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
-/
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
  */
-int main(void)
-{
-  int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-  int count, sz;
 
-  sz = sizeof(str) / sizeof(int);
-  for (count = 0; count < sz; count++)
-  {
-    _putchar(str[count]);
-  }
-  _putchar('\n');
-  return (0);
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
 }
