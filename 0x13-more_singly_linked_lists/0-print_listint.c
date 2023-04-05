e70e1ad45831de6fd189e1d342f73c69a9fecd88
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2023-04-05 01:09:53 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,14 @@
 #include "lists.h"
+/**
+ * author tinashetrishsamupindi
+ * Write a function that prints all the elements of a listint_t list.
+ * this function prints the list of integers and it returns the elements
+*/
 
-size_t print_listint(const listint_t *h);{
+size_t print_listint(const listint_t *h)
+{
 	int b;
 	for( b = 0; h; h = (*h).next, b++)
-		printf("%d/n, (*h).n) ;
+		printf("%d/n, (*h).n);
 	return (b);
 }
