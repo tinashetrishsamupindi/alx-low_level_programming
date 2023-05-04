92af86b68a76d37e078bf6319a5167a0d4e80c2a
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2023-05-04 15:59:19 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,19 @@
  * Return: the converted number, or 0 if an error occurs
  */
 unsigned int binary_to_uint(const char *b)
-{
-    unsigned int result = 0;
+int i;
+	unsigned int dec_val = 0;
 
-    if (b == NULL)
-        return 0;
+	if (!b)
+		return (0);
 
-    for (int i = 0; b[i] != '0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return 0;
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-        result = (result << 1) | (b[i] - '0');
-    }
-
-    return result;
+	return (dec_val);
 }
 
