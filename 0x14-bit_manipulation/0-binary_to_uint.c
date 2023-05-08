9d65d012ca308563cfff9a994dd530db0cb89bfa
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2023-05-08 00:10:23 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,31 @@
-#include "main.h"
+#include "main.h" 
+  
+ /** 
+  * binary_to_uint -converts a binary number to an unsigned int. 
+  * @b: pointing to a string of 0 and 1 chars 
+  * 
+  * Return:converted number, or 0 
+  */ 
+ unsigned int binary_to_uint(const char *b) 
+ { 
+         int i; 
+         unsigned int k; 
+  
+         k = 0; 
+         if (!b) 
+                 return (0); 
+         for (i = 0; b[i] != '\0'; i++) 
+         { 
+                 if (b[i] != '0' && b[i] != '1') 
+                         return (0); 
+         } 
+         for (i = 0; b[i] != '\0'; i++) 
+         { 
+                 k <<= 1; 
+                 if (b[i] == '1') 
+                         k += 1; 
+         } 
+         return (k); 
+ }
 
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number, or 0 if an error occurs
- */
-unsigned int binary_to_uint(const char *b)
-int i;
-	unsigned int dec_val = 0;
-
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-
-	return (dec_val);
-}
 
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,12 +7,13 @@
  *
  * Return: 1 for success, -1 for failure
  */
-int set_bit(unsigned long int *n, unsigned int index)
-{
-    if (index > 63)
-        return (-1);
-
-    *n = ((1UL << index) | *n);
-    return (1);
-}
-
+int clear_bit(unsigned long int *n, unsigned int index) 
+ { 
+         int mask; 
+  
+         if (index > 63 || !n) 
+                 return (-1); 
+         mask = 1 << index; 
+         *n = (*n & ~mask) | ((0 << index) & mask); 
+         return (1); 
+ }
